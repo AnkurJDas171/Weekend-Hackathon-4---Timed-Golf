dcdf1d35225a9f7db034bf69a4f00f00352ceee1
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-15 03:18:20 +0530

    some functions added

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,50 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = { time: 0, x: 0, y: 0, isStart: false };
+
+    this.handelStart = this.handelStart.bind(this);
+    this.statrTimer = this.statrTimer.bind(this);
   }
+
+  handelStart() {
+    console.log("button clicked");
+    this.setState({ isStart: true });
+
+    console.log(this.state);
+  }
+
+  statrTimer() {
+    console.log("time changed");
+    let time = this.state.isStart ? this.state.time + 1 : this.state.time;
+    this.setState({ time: time });
+  }
+
   componentDidMount() {
-    
+    this.timer = setInterval(() => this.statrTimer(), 1000);
   }
 
   componentWillUnmount() {
-    
+    clearInterval(this.timer);
   }
-
-
 
   render() {
     return (
- <>
-</>
+      <>
+        {!this.state.isStart && (
+          <button className="start" onClick={this.handelStart}>
+            start
+          </button>
+        )}
+
+        {this.state.isStart && (
+          <>
+            <div className="ball"></div>
+            <div className="hole"></div>
+            <div className="heading-timer">{this.state.time}</div>
+          </>
+        )}
+      </>
     );
   }
 }
