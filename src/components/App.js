6ac034564347fdb26c2dceb58358c6c1ffba05e9
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-15 12:21:42 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,94 +3,64 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0, isStart: false };
-
-    this.handelStart = this.handelStart.bind(this);
-    this.statrTimer = this.statrTimer.bind(this);
-    this.handelKeyDown = this.handelKeyDown.bind(this);
-    this.checkIfReachedDestination = this.checkIfReachedDestination.bind(this);
-  }
-
-  checkIfReachedDestination() {
-    if (this.state.x === 250 && this.state.y === 250) {
-      clearInterval(this.timer);
-      this.setState({ isStart: false });
-    }
-  }
-
-  handelStart() {
-    console.log("button clicked");
-    this.setState({ isStart: true });
-    this.timer = setInterval(() => this.statrTimer(), 1000);
-
-    console.log(this.state);
-  }
-
-  statrTimer() {
-    console.log("time changed");
-    if (!this.state.isStart) {
-      return;
-    }
-    this.setState({ time: this.state.time + 1 });
-  }
-
-  handelKeyDown(event) {
-    if (!this.state.isStart) {
-      return;
-    }
-
-    if (event.keyCode === 39) {
-      //right
-
-      let newX = this.state.x + 5;
-      this.setState({ x: newX });
-      this.checkIfReachedDestination();
-    } else if (event.keyCode === 40) {
-      //down
-
-      let newY = this.state.y + 5;
-      this.setState({ y: newY });
-      this.checkIfReachedDestination();
-    } else if (event.keyCode === 38) {
-      //up
-
-      let newY = this.state.y - 5;
-      this.setState({ y: newY });
-      this.checkIfReachedDestination();
-    } else if (event.keyCode === 37) {
-      //left
-
-      let newX = this.state.x - 5;
-      this.setState({ x: newX });
-      this.checkIfReachedDestination();
-    }
+    this.timer = 0;
+    this.state = { time: 0, x: 0, y: 0, isGameStarted: false };
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.handleStartGame = this.handleStartGame.bind(this);
   }
 
   componentDidMount() {
-    document.addEventListener("keydown", this.handelKeyDown);
+    document.addEventListener("keydown", this.onKeyDown, false);
+  }
+
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.timer);
+      document.removeEventListener("keydown", this.onKeyDown);
+    }
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown");
+    clearInterval(this.timer);
+    document.removeEventListener("keydown", this.onKeyDown);
+  }
+
+  onKeyDown(event) {
+    if (!this.state.isGameStarted) {
+      return;
+    }
+    if (event.keyCode === 39) {
+      this.setState({ x: this.state.x + 5 });
+    } else if (event.keyCode === 37) {
+      this.setState({ x: this.state.x - 5 });
+    } else if (event.keyCode === 38) {
+      this.setState({ y: this.state.y - 5 });
+    } else if (event.keyCode === 40) {
+      this.setState({ y: this.state.y + 5 });
+    }
+  }
+
+  handleStartGame() {
+    this.setState({ isGameStarted: true });
+    this.timer = setInterval(() => {
+      if (this.state.isGameStarted) {
+        this.setState({ time: this.state.time + 1 });
+      }
+    }, 1000);
   }
 
   render() {
     return (
       <>
-        <button
-          className="start"
-          onClick={this.handelStart}
-          disable={this.state.isStart}
-        >
+        <button className="start" onClick={this.handleStartGame}>
           start
         </button>
-
+        <div className="heading-timer">{this.state.time}</div>
         <div
           className="ball"
-          style={{ left: `${this.state.x}px`, top: `${this.state.y}px` }}
+          style={{ left: this.state.x + "px", top: this.state.y + "px" }}
         ></div>
         <div className="hole"></div>
-        <div className="heading-timer">{this.state.time}</div>
       </>
     );
   }
